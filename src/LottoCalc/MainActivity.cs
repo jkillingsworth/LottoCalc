b3333d176d5fbc74a50894016b4fd859eb0a731e
Jim Killingsworth <jkillingsworth@gmail.com>
2017-02-07 18:37:54 -0500

    Update application name

--- src/LottoCalc/MainActivity.cs
+++ src/LottoCalc/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.Widget;
 
 namespace LottoCalc
 {
-    [Activity(Label = "LottoCalc", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
