71d3e862d9c349e130ed52e19fbdb54292764121
Jim Killingsworth <jkillingsworth@gmail.com>
2017-02-06 23:57:42 -0500

    Implement basic functionality

--- src/LottoCalc/MainActivity.cs
+++ src/LottoCalc/MainActivity.cs
@@ -1,5 +1,9 @@
-﻿using Android.App;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android.App;
 using Android.OS;
+using Android.Widget;
 
 namespace LottoCalc
 {
@@ -11,6 +15,56 @@ namespace LottoCalc
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
+
+            var buttonGenerate = FindViewById<Button>(Resource.Id.buttonGenerate);
+            var buttonReset = FindViewById<Button>(Resource.Id.buttonReset);
+
+            buttonGenerate.Click += delegate { Generate(); };
+            buttonReset.Click += delegate { Reset(); };
+        }
+
+        private void Generate()
+        {
+            var textNumbers = FindViewById<TextView>(Resource.Id.textNumbers);
+            var buttonReset = FindViewById<Button>(Resource.Id.buttonReset);
+
+            textNumbers.Text = string.Join("  ", GetNumbers().Select(x => x.ToString()));
+            buttonReset.Enabled = true;
+        }
+
+        private void Reset()
+        {
+            var textNumbers = FindViewById<TextView>(Resource.Id.textNumbers);
+            var buttonReset = FindViewById<Button>(Resource.Id.buttonReset);
+
+            textNumbers.Text = "";
+            buttonReset.Enabled = false;
+        }
+
+        private List<int> GetNumbers()
+        {
+            const int poolCount = 53;
+            const int pickCount = 6;
+
+            var poolValues = new List<int>(poolCount);
+            var pickValues = new List<int>(pickCount);
+
+            var random = new Random();
+
+            for (int i = 0; i < poolCount; i++)
+            {
+                poolValues.Add(i + 1);
+            }
+
+            for (int i = 0; i < pickCount; i++)
+            {
+                var index = random.Next(poolValues.Count);
+                var value = poolValues[index];
+                poolValues.RemoveAt(index);
+                pickValues.Add(value);
+            }
+
+            return pickValues;
         }
     }
 }
--- src/LottoCalc/Resources/Resource.Designer.cs
+++ src/LottoCalc/Resources/Resource.Designer.cs
@@ -57,6 +57,28 @@ namespace LottoCalc
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int buttonGenerate = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int buttonReset = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int textNumbers = 2131034114;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
